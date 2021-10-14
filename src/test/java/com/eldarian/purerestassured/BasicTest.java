4eacef1d0b3665474207d6a1161dd21f254a393d
Dmitry Plyshevsky <eldarian3@gmail.com>
2021-10-14 21:18:33 +0300

    bdd test in rest assured

--- src/test/java/com/eldarian/purerestassured/BasicTest.java
+++ src/test/java/com/eldarian/purerestassured/BasicTest.java
@@ -5,6 +5,9 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class BasicTest {
 
     @Test
@@ -12,5 +15,17 @@ public class BasicTest {
         Response response = RestAssured.get("https://reqres.in/api/users?page=2");
         System.out.println(response.asString());
         Assert.assertEquals(response.statusCode(), 200);
+
+    }
+
+    @Test
+    public void testBDDGetRequest() {
+        given().
+                param("text", "test").
+        when().
+                get("http://md5.jsontest.com").
+        then().
+                assertThat().body("md5", equalTo("098f6bcd4621d373cade4e832627b4f6"));
+
     }
 }
